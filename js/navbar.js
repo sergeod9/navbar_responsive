cf25171127427dd4512dd809656307a7b130ee84
Georges Dahdouh <georges.dahdouh@gmail.com>
2020-05-07 08:08:03 -0700

    First commit
    This is first commit

---
+++ js/navbar.js
@@ -0,0 +1,132 @@
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const dropDownMenuArr = document.querySelectorAll('.dropdown');
+let breakPointArr;
+let breakPointSmall;
+let toggleHamburger = true;
+let dropdownMenuState = false;
+
+// Get screen sizes break points from json file
+/* That was a bad idea.. canceled, hard coded next..
+
+async function GetSizes(){
+    try{
+        const resp = await fetch("../json/screen-sizes.json");
+        breakPointSmall = await resp.json();
+        breakPointSmall = breakPointSmall.small;
+        console.log(breakPointSmall);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+// Get the screen sizes break points when document is loaded
+window.onload = GetSizes();
+
+
+if(window.innerWidth > breakPointSmall){
+    dropdownMenuState = true;
+    console.log(dropdownMenuState);
+    console.log("Hello");
+    
+}else{
+    console.log("wtf");
+    console.log(innerWidth);
+    console.log(breakPointSmall);
+}
+*/
+
+breakPointArr = {
+    "small":768,
+    "medium": 1024,
+    "large": 1280,
+    "xlarge": 1366 
+}
+breakPointSmall = breakPointArr.small;
+
+
+if(window.innerWidth > breakPointSmall){
+    dropdownMenuState = true;
+    console.log(breakPointSmall);
+}else{
+    dropdownMenuState = false;
+}
+
+// Create the arrwo down image element for the drop down menu
+const arrowDown = document.createElement('span');
+arrowDown.innerHTML = `<i class="dropdown-triangle"></i>`;
+
+const dropdownElement = document.querySelectorAll('.dropdown');
+
+// Add the arrow down image to all drop down elements
+dropdownElement.forEach(element => {
+    element.innerHTML += arrowDown.innerHTML;
+})
+
+// Response to screen size change
+window.addEventListener('resize', () => {
+    if(window.innerWidth > breakPointSmall){
+        menu.style.display = 'inline-block';
+        dropdownMenuState = true;
+
+    }else{
+        menu.style.display = 'none';
+        toggleHamburger = true;
+        dropdownMenuState = false;
+
+    }
+});
+
+// Toggle Hamburger show/hide menu for small window width
+hamburger.addEventListener('click', e =>{
+    if(toggleHamburger){
+        menu.style.display = 'inline-block';
+        toggleHamburger = false;
+    } else {
+        menu.style.display = 'none';
+        toggleHamburger =  true;
+    }
+})
+
+// Toggle dropdown menu show/hide
+dropDownMenuArr.forEach(dropdown => {
+    // Check if there are drop down menus in the navbar
+    if(dropdown){
+        let toggleDropdown = true;
+        if (dropdown.children.length > 0){
+            // menu should return the ul element
+            const menu = Array.from(dropdown.children).filter(child => child.className === "dropdown-menu")[0];
+            // Show dropdown menu when clicked
+            dropdown.addEventListener('click',()=>{
+                if (toggleDropdown && menu){
+                    
+                    
+                    menu.style.display = "block";
+                    if(dropdownMenuState === false){
+                        menu.style.position = "relative";
+                        menu.style.width = 100+"%";
+                    }
+                    else{
+                        menu.style.position = "absolute";
+                        menu.style.width = "max-content";
+                    }
+                    toggleDropdown = false;
+                    dropdown.addEventListener('mouseleave',()=>{
+                        menu.style.display = "none";
+                        toggleDropdown = true;
+                    })
+                }
+                else {
+                    menu.style.display = "none";
+                    toggleDropdown = true;
+                }
+                
+            });
+        }
+    }
+})
+
+
+
+
